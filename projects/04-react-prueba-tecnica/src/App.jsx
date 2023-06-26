281dbcb307c59be28692da3064e7152b97e9ec5d
RaulReact <aprendereactraul@gmail.com>
2023-06-26 19:38:02 +0200

    Added tests with Playwright Library

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -16,15 +16,12 @@ export function App() {
 
     return (
         <main>
-            <h1>App de Gatitos</h1>
-            <button onClick={handleClick}>Get New Fact</button>
-            <section>
-                
-                {fact && <p>{fact}</p>}
-                {imageUrl && <img src={imageUrl} alt={`Image extrated using the first
-                three words for ${fact}`} />}
-            </section>
-            <Otro/>
-        </main>
+        <h1>App de gatitos</h1>
+  
+        <button onClick={handleClick}>Get new fact</button>
+  
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={imageUrl} alt={`Image extracted using the first rhee words for ${fact}`} />}
+      </main>
     )
 }
