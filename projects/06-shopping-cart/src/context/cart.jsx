80d3ac26219fc682c4fb17dbaa18c13ffc9cb44e
RaulReact <aprendereactraul@gmail.com>
2023-07-29 14:35:51 +0200

    Implementando reducer

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -1,48 +1,71 @@
-import { createContext,useState } from "react";
+import { createContext,useReducer,useState } from "react";
 
+const initialState = []
+// Reducer - Transforma el estado (state) a traves de una accion(action) a partir
+// de un nuevo estado. Devuelve un estado
+
+const reducer = (state,action)=>{
+    const {type:actionType, payload: actionPayload} = action
+    switch(action.type){
+        case 'ADD_TO_CART':{
+            const {id} = actionPayload
+            const productInCartIndex = state.findIndex(item => item.id === id)
+
+            if(productInCartIndex >= 0){
+                //Una forma seria usando "structuredClone" - Nos permite hacer una copia profunda de un array
+                const newState = structuredClone(state)
+                newState[productInCartIndex].quantity += 1
+                return newState
+            }
+
+            return [
+                ...state,
+                {
+                    ...actionPayload, // product
+                    quantity:1
+                }
+            ]
+        }
+        case 'REMOVE_FROM_CART':{
+            const {id} = actionPayload
+            return state.filter(item => item.id !== id)
+        }
+
+        case 'CLEAR_CART': {
+            return initialState;
+        }
+    }
+
+    return state
+}
 // 1. Crear Contexto
 export const CartContext = createContext() //Singleton - Solo se llama una vez
 
 // 2. Crear Provider
 // Los children es lo que envolvera
 export function CartProvider ({children}){
-    const [cart,setCart] = useState([]);
-    const addToCart = product =>{
-         //Check if the product already in the cart
-        const productCartIndex = cart.findIndex(item => item.id === product.id)
-        if(productCartIndex >= 0){
-            //Una forma seria usando "structuredClone" - Nos permite hacer una copia profunda de un array
-            const newCart = structuredClone(cart)
-            newCart[productCartIndex].quantity += 1
-            return setCart(newCart)
-        }
-
-        //If product is not in the cart
-
-        setCart(prevState => ([
-            prevState,
-            {
-                ...product,
-                quantity:1
-            }
-        ]))
-
-    }
-
-    //Delete product from the cart
-    const removeFromCart = product =>{
-        setCart(prevState => prevState.filter(item => item.id !== product.id))
-    }
-
-    //Delete all the products from the cart
     
-    const clearCart= () => { setCart([
+    //Una vez que tenemos creado el reducer utilizamos el hook "useReducer"
+    //Aqui algo muy interesante porque aparece el metodo o la funcion dispatch
+    // que va a ser el encargado de enviar las acciones a el reducer
+    
+    const [state,dispatch] = useReducer(reducer,initialState)
 
-    ])}
+    const addToCart = product => dispatch({
+        type:'ADD_TO_CART',
+        payload: product
+    })
+
+    const removeFromCart = product => dispatch({
+        type:'REMOVE_FROM_CART',
+        payload: product
+    })
+
+    const clearCart = () => dispatch({type:'CLEAN_CART'})
 
     return(
         <CartContext.Provider value = {{
-            cart,
+            cart:state,
             addToCart,
             removeFromCart,
             clearCart
